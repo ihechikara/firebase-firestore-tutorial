e6315592ea608b085d8a5d66de1946e3932281e6
ihechikara <ihechivinabba@gmail.com>
2021-10-08 16:59:54 -0700

    order and limit data branch

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-db.collection("userInfo").where('username', '==', 'Ihechikara')
+//Order data
+db.collection("userInfo").orderBy('username')
   .get()
   .then((querySnapshot) => {
     querySnapshot.forEach(doc =>{
         console.log(doc.data())
     })
+  })
+  .catch((error) => {
+    console.log("Error getting documents: ", error);
+  });
+
+//limit data
+db.collection("userInfo").limit(1)
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
   })
   .catch((error) => {
     console.log("Error getting documents: ", error);
