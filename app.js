a9ac7de67121a8b1f9aad571eec3f54a56caf96e
ihechikara <ihechivinabba@gmail.com>
2021-10-08 14:48:10 -0700

    added queries branch

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-const lists = document.getElementById("lists");
-
-db.collection("userInfo")
+db.collection("userInfo").where('username', '==', 'Ihechikara')
   .get()
   .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      let li = document.createElement("li");
-      let username = document.createElement("h4");
-      let bio = document.createElement("p");
-
-      username.textContent = doc.data().username;
-      bio.textContent = doc.data().bio;
-
-      li.appendChild(username);
-      li.appendChild(bio);
-
-      lists.appendChild(li);
-    });
+    querySnapshot.forEach(doc =>{
+        console.log(doc.data())
+    })
   })
   .catch((error) => {
     console.log("Error getting documents: ", error);
